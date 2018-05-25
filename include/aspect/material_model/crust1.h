2eaf2397d14ca022ea628bb3f6088297263be926
naliboff <jbnaliboff@ucdavis.edu>
2018-05-25 16:45:48 -0700

    Add crust1 material model

---
+++ include/aspect/material_model/crust1.h
@@ -0,0 +1,214 @@
+/*
+  Copyright (C) 2014 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __aspect__model_crust1_h
+#define __aspect__model_crust1_h
+
+#include <aspect/material_model/interface.h>
+#include <aspect/simulator_access.h>
+#include <deal.II/base/parameter_handler.h>
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    using namespace dealii;
+
+    /**
+<<<<<<< HEAD
+     * This model is desgined specifically to be used when CRUST1.0 defines"
+     * the compositional initial conditions.  Currently, the model only uses"
+     * one compositional field where the value at each point represents a"
+     * specific rock type in the CRUST1.0 model.  The user should specify 10"
+     * density values in the input file, which represent air, water, ice,"
+     * sed layer 1, sed layer 2, sed layer 3, upper crust, middle crust,"
+     * lower crust and mantle.
+=======
+     * A material model which is intended for use with multiple compositional
+     * fields. Each compositional field is meant to be a single rock type,
+     * where the value of the field at a point is interpreted to be a volume
+     * fraction of that rock type.  If the sum of the compositional field
+     * volume fractions is less than one, then the remainder of the volume is
+     * assumed to be ``background mantle''.  If the sum of the compositional
+     * field volume fractions is greater than one, then they are renormalized
+     * to sum to one and there is no background mantle.
+     *
+     * For each material parameter the user supplies a comma delimited list of
+     * length N+1, where N is the number of compositional fields.  The
+     * additional field corresponds to the value for background mantle.  They
+     * should be ordered ``background, composition1, composition2...''
+     *
+     * If a single value is given, then all the compositional fields are given
+     * that value. Other lengths of lists are not allowed.  For a given
+     * compositional field the material parameters are treated as constant,
+     * except density, which varies linearly with temperature according to the
+     * thermal expansivity.
+     *
+     * When more than one field is present at a point, they are averaged
+     * arithmetically. An exception is viscosity, which may be averaged
+     * arithmetically, harmonically, geometrically, or by selecting the
+     * viscosity of the composition with the greatest volume fraction.
+     *
+>>>>>>> 5a81297... files added this time, bah!
+     * @ingroup MaterialModels
+     */
+    template <int dim>
+    class Crust1 : public MaterialModel::Interface<dim>, public ::aspect::SimulatorAccess<dim>
+    {
+      public:
+
+        /**
+         * Function to compute the material properties in @p out given the
+         * inputs in @p in. If MaterialModelInputs.strain_rate has the length
+         * 0, then the viscosity does not need to be computed.
+         */
+        virtual void evaluate(const MaterialModel::MaterialModelInputs<dim> &in,
+                              MaterialModel::MaterialModelOutputs<dim> &out) const;
+
+        /**
+         * @name Qualitative properties one can ask a material model
+         * @{
+         */
+
+        /**
+         * This model is not compressible, so this returns false.
+         */
+        virtual bool is_compressible () const;
+        /**
+         * @}
+         */
+
+        /**
+         * @name Reference quantities
+         * @{
+         */
+        virtual double reference_viscosity () const;
+
+        virtual double reference_density () const;
+
+        virtual double reference_thermal_expansion_coefficient () const;
+
+        double reference_thermal_diffusivity () const;
+
+        double reference_cp () const;
+        /**
+         * @}
+         */
+
+        /**
+         * @name Functions used in dealing with run-time parameters
+         * @{
+         */
+
+        /**
+         * Declare the parameters this class takes through input files.
+         */
+        static
+        void
+        declare_parameters (ParameterHandler &prm);
+
+        /**
+         * Read the parameters this class declares from the parameter file.
+         */
+        virtual
+        void
+        parse_parameters (ParameterHandler &prm);
+        /**
+         * @}
+         */
+
+      private:
+        /**
+<<<<<<< HEAD
+=======
+         * From a list of compositional fields of length N, we come up with an
+         * N+1 length list that which also includes the fraction of
+         * ``background mantle''. This list should sum to one, and is
+         * interpreted as volume fractions.  If the sum of the
+         * compositional_fields is greater than one, we assume that there is
+         * no background mantle (i.e., that field value is zero).  Otherwise,
+         * the difference between the sum of the compositional fields and 1.0
+         * is assumed to be the amount of background mantle.
+         */
+        const std::vector<double> compute_volume_fractions(
+          const std::vector<double> &compositional_fields) const;
+        /**
+>>>>>>> 5a81297... files added this time, bah!
+         * Reference temperature for thermal expansion.  All components use
+         * the same reference_T.
+         */
+        double reference_T;
+
+        /**
+<<<<<<< HEAD
+=======
+        * Enumeration for selecting which averaging scheme to use.
+        * Select between harmonic, arithmetic, geometric, and
+        * maximum_composition.  The max composition scheme simply uses the
+        * parameter of whichever field has the highest volume fraction.
+        */
+        enum AveragingScheme
+        {
+          harmonic,
+          arithmetic,
+          geometric,
+          maximum_composition
+        };
+
+
+        AveragingScheme viscosity_averaging;
+
+        double average_value (const std::vector<double> &composition,
+                              const std::vector<double> &parameter_values,
+                              const enum AveragingScheme &average_type) const;
+
+
+        /**
+>>>>>>> 5a81297... files added this time, bah!
+         * Vector for field densities, read from parameter file .
+         */
+        std::vector<double> densities;
+
+        /**
+         * Vector for field viscosities, read from parameter file.
+         */
+        std::vector<double> viscosities;
+
+        /**
+         * Vector for field thermal expnsivities, read from parameter file.
+         */
+        std::vector<double> thermal_expansivities;
+
+        /**
+         * Vector for field thermal conductivities, read from parameter file.
+         */
+        std::vector<double> thermal_conductivities;
+
+        /**
+         * Vector for field specific heats, read from parameter file.
+         */
+        std::vector<double> specific_heats;
+    };
+
+  }
+}
+
+#endif
---
+++ source/material_model/crust1.cc
@@ -0,0 +1,264 @@
+/*
+  Copyright (C) 2011 - 2015 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/material_model/crust1.h>
+#include <aspect/simulator.h>
+#include <aspect/utilities.h>
+
+#include <numeric>
+
+using namespace dealii;
+
+namespace aspect
+{
+  namespace MaterialModel
+  {
+
+    template <int dim>
+    void
+    Crust1<dim>::
+    evaluate(const MaterialModel::MaterialModelInputs<dim> &in,
+             MaterialModel::MaterialModelOutputs<dim> &out) const
+    {
+      for (unsigned int i=0; i < in.temperature.size(); ++i)
+        {
+          const double temperature = in.temperature[i];
+          const std::vector<double> composition = in.composition[i];
+
+          out.viscosities[i] = viscosities[0];
+          out.specific_heat[i] = specific_heats[0];
+          out.thermal_conductivities[i] = thermal_conductivities[0];
+
+          // composition
+          const double c = (in.composition[i].size()>0)
+                           ?
+                           std::max(0.0, in.composition[i][0])
+                           :
+                           0.0;
+          unsigned int com = 0;
+          for (unsigned int j=0; j < densities.size(); ++j)
+            {
+              if ((unsigned int)c == j)
+                {
+                  com = j;
+                }
+            }
+          out.densities[i] = densities[com];
+<<<<<<< HEAD
+=======
+          //std::cout << c << "," << com << "," << out.densities[i]  << std::endl;
+>>>>>>> 5a81297... files added this time, bah!
+
+          out.thermal_expansion_coefficients[i] = thermal_expansivities[0];
+
+          // Compressibility at the given positions.
+          // The compressibility is given as
+          // $\frac 1\rho \frac{\partial\rho}{\partial p}$.
+          // (here we use an incompressible medium)
+          out.compressibilities[i] = 0.0;
+          // Pressure derivative of entropy at the given positions.
+          out.entropy_derivative_pressure[i] = 0.0;
+          // Temperature derivative of entropy at the given positions.
+          out.entropy_derivative_temperature[i] = 0.0;
+          // Change in composition due to chemical reactions at the
+          // given positions. The term reaction_terms[i][c] is the
+          // change in compositional field c at point i.
+          for (unsigned int c=0; c<in.composition[i].size(); ++c)
+            out.reaction_terms[i][c] = 0.0;
+
+        }
+    }
+
+    template <int dim>
+    double
+    Crust1<dim>::
+    reference_viscosity () const
+    {
+      return viscosities[0]; //background
+    }
+
+    template <int dim>
+    double
+    Crust1<dim>::
+    reference_density () const
+    {
+      return densities[0];  //background
+    }
+
+    template <int dim>
+    double
+    Crust1<dim>::
+    reference_thermal_expansion_coefficient () const
+    {
+      return thermal_expansivities[0]; //background
+    }
+
+    template <int dim>
+    double
+    Crust1<dim>::
+    reference_cp () const
+    {
+      return specific_heats[0]; //background
+    }
+
+    template <int dim>
+    double
+    Crust1<dim>::
+    reference_thermal_diffusivity () const
+    {
+      return thermal_conductivities[0] /( densities[0]* specific_heats[0] ); //background
+    }
+
+    template <int dim>
+    bool
+    Crust1<dim>::
+    is_compressible () const
+    {
+      return false;
+    }
+
+    template <int dim>
+    void
+    Crust1<dim>::declare_parameters (ParameterHandler &prm)
+    {
+      prm.enter_subsection("Material model");
+      {
+        prm.enter_subsection("Crust1");
+        {
+          prm.declare_entry ("Reference temperature", "293",
+                             Patterns::Double (0),
+                             "The reference temperature $T_0$. Units: $K$.");
+          prm.declare_entry ("Densities", "3300.",
+                             Patterns::List(Patterns::Double(0)),
+                             "List of densities for background mantle and compositional fields,"
+                             "for a total of N+1 values, where N is the number of compositional fields."
+                             "If only one value is given, then all use the same value.  Units: $kg / m^3$");
+          prm.declare_entry ("Viscosities", "1.e21",
+                             Patterns::List(Patterns::Double(0)),
+                             "List of viscosities for background mantle and compositional fields,"
+                             "for a total of N+1 values, where N is the number of compositional fields."
+                             "If only one value is given, then all use the same value. Units: $Pa s$");
+          prm.declare_entry ("Thermal expansivities", "4.e-5",
+                             Patterns::List(Patterns::Double(0)),
+                             "List of thermal expansivities for background mantle and compositional fields,"
+                             "for a total of N+1 values, where N is the number of compositional fields."
+                             "If only one value is given, then all use the same value. Units: $1/K$");
+          prm.declare_entry ("Specific heats", "1250.",
+                             Patterns::List(Patterns::Double(0)),
+                             "List of specific heats $C_p$ for background mantle and compositional fields,"
+                             "for a total of N+1 values, where N is the number of compositional fields."
+                             "If only one value is given, then all use the same value. Units: $J /kg /K$");
+          prm.declare_entry ("Thermal conductivities", "4.7",
+                             Patterns::List(Patterns::Double(0)),
+                             "List of thermal conductivities for background mantle and compositional fields,"
+                             "for a total of N+1 values, where N is the number of compositional fields."
+                             "If only one value is given, then all use the same value. Units: $W/m/K$ ");
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+    }
+
+    template <int dim>
+    void
+    Crust1<dim>::parse_parameters (ParameterHandler &prm)
+    {
+      //not pretty, but we need to get the number of compositional fields before
+      //simulatoraccess has been initialized here...
+      unsigned int n_foreground_fields;
+      prm.enter_subsection ("Compositional fields");
+      {
+        n_foreground_fields = prm.get_integer ("Number of fields");
+      }
+      prm.leave_subsection();
+
+      const unsigned int n_fields= n_foreground_fields + 1;
+      const unsigned int d_fields= 10;
+
+
+      prm.enter_subsection("Material model");
+      {
+        prm.enter_subsection("Crust1");
+        {
+          reference_T = prm.get_double ("Reference temperature");
+          
+          // Parse multicomponent properties
+          densities = Utilities::possibly_extend_from_1_to_N (Utilities::string_to_double(Utilities::split_string_list(prm.get("Densities"))),
+                                                              d_fields,
+                                                              "Densities");
+          viscosities = Utilities::possibly_extend_from_1_to_N (Utilities::string_to_double(Utilities::split_string_list(prm.get("Viscosities"))),
+                                                                n_fields,
+                                                                "Viscosities");
+          thermal_conductivities = Utilities::possibly_extend_from_1_to_N (Utilities::string_to_double(Utilities::split_string_list(prm.get("Thermal conductivities"))),
+                                                                           n_fields,
+                                                                           "Thermal conductivities");
+          thermal_expansivities = Utilities::possibly_extend_from_1_to_N (Utilities::string_to_double(Utilities::split_string_list(prm.get("Thermal expansivities"))),
+                                                                          n_fields,
+                                                                          "Thermal expansivities");
+          specific_heats = Utilities::possibly_extend_from_1_to_N (Utilities::string_to_double(Utilities::split_string_list(prm.get("Specific heats"))),
+                                                                   n_fields,
+                                                                   "Specific heats");
+
+        }
+        prm.leave_subsection();
+      }
+      prm.leave_subsection();
+
+      // Declare dependencies on solution variables
+      this->model_dependence.viscosity = NonlinearDependence::compositional_fields;
+      this->model_dependence.density = NonlinearDependence::temperature | NonlinearDependence::compositional_fields;
+      this->model_dependence.compressibility = NonlinearDependence::none;
+      this->model_dependence.specific_heat = NonlinearDependence::compositional_fields;
+      this->model_dependence.thermal_conductivity = NonlinearDependence::compositional_fields;
+    }
+  }
+}
+
+// explicit instantiations
+namespace aspect
+{
+  namespace MaterialModel
+  {
+    ASPECT_REGISTER_MATERIAL_MODEL(Crust1,
+                                   "crust1",
+<<<<<<< HEAD
+                                   "This model is desgined specifically to be used when CRUST1.0 defines"
+                                   " the compositional initial conditions.  Currently, the model only uses"
+                                   " one compositional field where the value at each point represents a"
+                                   " specific rock type in the CRUST1.0 model.  The user should specify 10"
+                                   " density values in the input file, which represent air, water, ice,"  
+                                   " sed layer 1, sed layer 2, sed layer 3, upper crust, middle crust,"
+                                   " lower crust and mantle.  At some point soon, density will be determined"
+                                   " from a separate compositional field that contains the CRUST1.0 densities" 
+                                   " read in from a file.")
+=======
+                                   "This model is for use with an arbitrary number of compositional fields, where each field"
+                                   " represents a rock type which can have completely different properties from the others."
+                                   " However, each rock type itself has constant material properties.  The value of the "
+                                   " compositional field is interpreed as a volume fraction. If the sum of the fields is"
+                                   " greater than one, they are renormalized.  If it is less than one, material properties "
+                                   " for ``background mantle'' make up the rest. When more than one field is present, the"
+                                   " material properties are averaged arithmetically.  An exception is the viscosity,"
+                                   " where the averaging should make more of a difference.  For this, the user selects"
+                                   " between arithmetic, harmonic, geometric, or maximum composition averaging.")
+>>>>>>> 5a81297... files added this time, bah!
+  }
+}
